d6657939a8529e9f95acfbd0538d40d9a5ca0168
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-05-07 10:15:02 -0300

    calculator in Java termined

--- simple console calculator in Java/Calculator.java
+++ simple console calculator in Java/Calculator.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        boolean stillPlaying = true;
+        char operator;
+        boolean stillCalc = true;
         double firstNumber, secondNumber;
         Scanner scanner = new Scanner(System.in);
 
@@ -11,20 +12,22 @@ public class Calculator {
             System.out.print("Insert the first number: ");
             firstNumber = scanner.nextDouble();
 
-            System.out.println(captureOp(scanner));
+            operator = captureOperator(scanner);
 
             System.out.println("\nInsert the operation:");
 
             System.out.print("\nInsert the second number: ");
             secondNumber = scanner.nextDouble();
 
-            System.out.printf("%2f + %2f == %2f", firstNumber, secondNumber, firstNumber + secondNumber);
-        }while(stillPlaying == true);
+            calcule(firstNumber, operator, secondNumber);
 
+            stillCalc = checkIfStillCalc(scanner);
+        }while(stillCalc == true);
+        System.out.print("Thanks for calculate!");
         scanner.close();
     }
 
-    static char captureOp(Scanner scanner){
+    static char captureOperator(Scanner scanner){
         char opSelected = ' ';
         String[][] operations = {
             {"+", "sum"},
@@ -47,10 +50,58 @@ public class Calculator {
                 if(opSelected != operations[i][0].charAt(0)) aux++;
             }
             if(aux == operations.length){
+                System.out.print("\nInsert a correct operation!\n\n");
                 opSelected = ' ';
             }
         }while(opSelected == ' ');
         return opSelected;
     }
-}
 
+    static boolean checkIfStillCalc(Scanner scanner){
+        char check;
+        System.out.print("\nDo you want to calculate again? [type 'y' or 'n']: ");
+        check = scanner.next().charAt(0);
+
+        while(check != 'y' && check != 'n'){
+            System.out.print("\nDo you want to calculate again? [type 'y' or 'n']: ");
+            check = scanner.nextLine().charAt(0);
+
+            System.out.print(check == 'n');
+
+            if(check != 'y' && check != 'n'){
+                System.out.println("You must type 'y' or 'n'!");
+            }
+        }
+
+        boolean still = true;
+
+        if(check == 'y') still = true;
+        if(check == 'n') still = false;
+
+        return still;
+    }
+
+    static void calcule(double numOne, char op, double numTwo){
+        double result = 0;
+
+        switch (op) {
+            case '+':
+                result = numOne + numTwo;
+                break;
+            case '-':
+                result = numOne - numTwo;
+                break;
+            case '*':
+                result = numOne * numTwo;
+                break;
+            case '/':
+                result = numOne / numTwo;
+                break;
+            case '%':
+                result = numTwo * (numOne / 100);
+                break;
+        }
+
+        System.out.printf("%2f %c %2f = %2f", numOne, op, numTwo, result);
+    }
+}
